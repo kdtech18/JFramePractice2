fb62cd81eebdb86bf75d1bed703af7bcec306f86
Kenton Duprey <kdtech18@gmail.com>
2016-11-16 13:19:16 -0500

    Initial commit

---
+++ src/JFramePractice2.java
@@ -0,0 +1,31 @@
+/* JFramePractice2.java
+   Kenton Duprey
+   Java Graphics II G
+   Mr. Blondin
+   11/16/2016
+*/
+
+import java.awt.Color;
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class JFramePractice2 extends JFrame
+    {
+        BorderLayout border = new BorderLayout();
+        
+        JPanel northPnl = new JPanel();
+        JPanel eastPnl = new JPanel();
+        JPanel southPnl = new JPanel();
+        JPanel westPnl = new JPanel();
+        JPanel centerPnl = new JPanel();
+        
+        JLabel northLbl = new JLabel("NORTH");
+        JLabel eastLbl = new JLabel("EAST");
+        JLabel westLbl = new JLabel("WEST");
+        JLabel southLbl = new JLabel("SOUTH");
+        JLabel centerLbl = new JLabel("CENTER");
+        
+        JButton pressMe = new JButton("Press me!");
+    
+    }
